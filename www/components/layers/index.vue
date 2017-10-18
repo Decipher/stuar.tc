232913dbe92f8e57ae710a31da73537145988aa9
Stuart Clark <stu@rtclark.net>
2017-10-19 07:57:58 +1100

    Updated vMediaQuery with Bootstrap breakpoints.

--- www/components/layers/index.vue
+++ www/components/layers/index.vue
@@ -14,7 +14,7 @@
 
     <!-- Controls - Pager component -->
     <scvr-controls-pager
-      v-if="!responsive.vr && ($mq.resize && $mq.below(576))"
+      v-if="!responsive.vr && ($mq.resize && $mq.below(this.$mv.sm))"
 
       @pagerNext="pagerNext()"
       @pagerPrev="pagerPrev()"
@@ -66,7 +66,7 @@
 
       // Pager - Position.
       pagerPos () {
-        if (!this.responsive.vr && this.$mq.resize && this.$mq.below(576)) {
+        if (!this.responsive.vr && this.$mq.resize && this.$mq.below(this.$mv.sm)) {
           // < SM: Y = 17.5.
           return `0 -23.5 17.5`
         }
@@ -83,7 +83,7 @@
         // Responsive positioning.
         if (!this.responsive.vr && this.$mq.resize) {
           // < SM: Y = ~17.5.
-          if (this.$mq.below(576)) {
+          if (this.$mq.below(this.$mv.sm)) {
             col = col + this.pagerOffset > 2 ? col + this.pagerOffset - 3 : col + this.pagerOffset
 
             return `0 ${(row - 1) * (10 + 1) + 16 + col * 2} ${17.5 - col * 2.5}`
@@ -114,7 +114,7 @@
 
         if (this.$mq.resize) {
           // < SM.
-          if (this.$mq.below(576)) {
+          if (this.$mq.below(this.$mv.sm)) {
             coordX = 12.5
             coordY = 11.15
             coordZ = 17.5
--- www/plugins/v-media-query.js
+++ www/plugins/v-media-query.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
 import vMediaQuery from 'v-media-query'
 
-Vue.use(vMediaQuery)
+Vue.use(vMediaQuery, {
+  variables: {
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200
+  }
+})
