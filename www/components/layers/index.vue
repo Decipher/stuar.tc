cf1ef59509e361e3c02d859de14c7934a2967dd0
Stuart Clark <stu@rtclark.net>
2017-10-23 21:16:13 +1100

    Updated Index mobile layout and added animation system.

--- www/components/layers/index.vue
+++ www/components/layers/index.vue
@@ -1,16 +1,32 @@
 <template>
   <a-entity v-if="delta === 0" id="index">
 
-    <!-- Entity teaser component. -->
-    <scvr-entity-teaser
-      v-for="(entity, uuid, delta) in $store.state.index"
+    <!-- Entity teaser components. -->
+    <a-entity
+      :position="wrapperPos()"
 
-      :delta="delta"
-      :disabled="disabled"
-      :entity="entity"
-      :key="uuid"
-      :position="teaserPos(delta)"
-      :rotation="teaserRot(delta)" />
+      id="teasers">
+      <scvr-entity-teaser
+        v-for="(entity, uuid, delta) in $store.state.index"
+
+        :delta="delta"
+        :disabled="disabled"
+        :entity="entity"
+        :key="uuid"
+        :position="teaserPos(delta)"
+        :rotation="teaserRot(delta)" />
+
+        <!-- Animation: Adjust teasers rotation as per pager. -->
+        <a-animation
+          v-if="animPlay"
+
+          @animationend='animPlay = false'
+
+          :attribute="animAttribute"
+          :from="`${animFrom} 0 0`"
+          :to="`${animTo} 0 0`" />
+
+      </a-entity>
 
     <!-- Controls - Pager component -->
     <scvr-controls-pager
@@ -51,17 +67,37 @@
 
     data () {
       return {
-        pagerOffset: 0
+        animAttribute: 'rotation',
+        animFrom: 0,
+        animPlay: false,
+        animTo: 0
       }
     },
 
     methods: {
-      pagerNext () {
-        this.pagerOffset = this.pagerOffset === 2 ? 0 : this.pagerOffset + 1
+      // Animate.
+      animate (from, to, attribute) {
+        this.animFrom = from
+        this.animTo = to
+        this.animAttribute = attribute
+        this.animPlay = true
       },
 
+      // Event - $mq.resize.
+      eventResize () {
+        if (this.$mq.above(this.$mv.sm)) {
+          this.animate(0, 0, 'rotation')
+        }
+      },
+
+      // Pager - Next event.
+      pagerNext () {
+        this.animate(this.animTo, this.animTo + 120, 'rotation')
+      },
+
+      // Pager - Previous event.
       pagerPrev () {
-        this.pagerOffset = this.pagerOffset === 0 ? 2 : this.pagerOffset - 1
+        this.animate(this.animTo, this.animTo - 120, 'rotation')
       },
 
       // Pager - Position.
@@ -84,9 +120,7 @@
         if (!this.responsive.vr && this.$mq.resize) {
           // < SM: Y = ~17.5.
           if (this.$mq.below(this.$mv.sm)) {
-            col = col + this.pagerOffset > 2 ? col + this.pagerOffset - 3 : col + this.pagerOffset
-
-            return `0 ${(row - 1) * (10 + 1) + 16 + col * 2} ${17.5 - col * 2.5}`
+            return `0 ${(row - 1) * (10 + 1) + 22} 17.5`
           }
 
           // >= SM: Y = -25.
@@ -100,6 +134,10 @@
       // Teaser - Rotation.
       teaserRot (delta) {
         if (!this.responsive.vr) {
+          if (this.$mq.below(this.$mv.sm)) {
+            return `${(delta % 3) * 120} 0 0`
+          }
+
           return `0 0 0`
         }
 
@@ -128,9 +166,25 @@
         }
 
         return `${coordX} ${coordY} ${coordZ}`
+      },
+
+      // Wrapper - Position.
+      wrapperPos () {
+        // < SM.
+        if (this.$mq.resize && this.$mq.below(this.$mv.sm)) {
+          return '0 -6 0'
+        }
+
+        // >= SM.
+        return '0 0 0'
       }
+
     },
 
-    props: ['data', 'delta']
+    props: ['data', 'delta'],
+
+    watch: {
+      '$mq.resize': 'eventResize'
+    }
   }
 </script>
