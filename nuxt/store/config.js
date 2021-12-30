91336897c539a5b73342678ce71cbf76af01a646
Stuart Clark <stuart@realityloop.com>
2021-12-30 13:47:43 +1100

    feat(#10): add Config pages Vuex store

--- nuxt/store/config.js
+++ nuxt/store/config.js
@@ -1,9 +1,33 @@
 export const state = () => ({
-  title: 'Stuart Clark',
+  title: undefined,
   social: {
-    drupal: 'Deciphered',
-    github: 'Decipher',
-    gravatar: '499831a65f45885a7e1b70ea47c06a58',
-    twitter: 'Decipher'
+    drupal: undefined,
+    github: undefined,
+    gravatar: undefined,
+    twitter: undefined
   },
 })
+
+export const mutations = {
+  set: (state, data) => {
+    state.title = data.title
+    state.social = data.social
+  }
+}
+
+export const actions = {
+  init: async ({ commit }, { $druxt }) => {
+    const collection = await $druxt.getCollection('config_pages--druxt_settings')
+    const entity = collection.data.shift()
+    const data = {
+      title: entity.attributes.field_site_name,
+      social: {
+        drupal: entity.attributes.field_social_drupal,
+        github: entity.attributes.field_social_github,
+        gravatar: entity.attributes.field_social_gravatar,
+        twitter: entity.attributes.field_social_twitter,
+      }
+    }
+    commit('set', data)
+  }
+}
