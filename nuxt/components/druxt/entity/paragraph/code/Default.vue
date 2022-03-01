1e9cff77064ea27e97648aeff57eca8016fb5e21
Stuart Clark <stu@rtclark.net>
2022-03-01 17:39:22 +1100

    feat(#7): add Code paragraph

---
+++ nuxt/components/druxt/entity/paragraph/code/Default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mockup-code mx-4 my-8">
+    <pre
+      v-if="filename"
+      class="mb-4"
+      data-prefix="#"
+    ><code v-text="filename" /></pre>
+
+    <pre
+      v-for="(line, lineNumber) of code"
+      :key="lineNumber"
+      :data-prefix="lineNumber"
+    ><!--
+      --><code v-text="line" /><!--
+    --></pre>
+  </div>
+</template>
+
+<script>
+import { DruxtEntityMixin } from 'druxt-entity'
+export default {
+  mixins: [DruxtEntityMixin],
+
+  computed: {
+    code: ({ entity }) => entity.attributes.field_code.split('\n'),
+    filename: ({ entity }) => entity.attributes.field_title,
+  },
+}
+</script>
