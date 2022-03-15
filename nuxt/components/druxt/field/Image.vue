b31ba4761a60d6d09ccc512786ee2e1a730784db
Stuart Clark <stu@rtclark.net>
2022-03-15 19:39:18 +1100

    chore(#55): update image styles

--- nuxt/components/druxt/field/Image.vue
+++ nuxt/components/druxt/field/Image.vue
@@ -1,7 +1,7 @@
 <template>
-  <DruxtEntity type="file--file" :uuid="model.data.id">
+  <DruxtEntity class="mx-4" type="file--file" :uuid="model.data.id">
     <template #default="{ entity }">
-      <NuxtImg class="w-full" :src="src(entity)" />
+      <NuxtImg class="w-full rounded-2xl" :src="src(entity)" />
     </template>
   </DruxtEntity>
 </template>
