82d702ad0a41a0089e05ca496fe8aebb75996431
Stuart Clark <stu@rtclark.net>
2022-03-15 19:27:44 +1100

    [#55] Add Nuxt image module (#56)
    * feat(#55): add and setup Nuxt image module
    
    * chore(#55): update nuxt/image config
    
    * chore(#55): add blog post
    
    * chore: add description to code paragraph
    
    * chore(#55): add missing content
    
    * chore(#55): update content
    
    * chore(#55): update theme components

--- nuxt/components/druxt/entity/paragraph/code/Default.vue
+++ nuxt/components/druxt/entity/paragraph/code/Default.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="mockup-code mx-4 my-8">
-    <pre
-      v-if="filename"
-      class="mb-4"
-      data-prefix="#"
-    ><code v-text="filename" /></pre>
+  <div class="my-8">
+    <div v-if="description" class="container mx-auto px-4 my-2">
+      <!-- eslint-disable-next-line vue/no-v-html -->
+      <div class="prose" v-html="description" />
+    </div>
+    <div class="mockup-code mx-4">
+      <pre
+        v-if="filename"
+        class="mb-4"
+        data-prefix="#"
+      ><code v-text="filename" /></pre>
 
-    <pre
-      v-for="(line, lineNumber) of code"
-      :key="lineNumber"
-      :data-prefix="lineNumber"
-    ><!--
-      --><code v-text="line" /><!--
-    --></pre>
+      <pre
+        v-for="(line, lineNumber) of code"
+        :key="lineNumber"
+        :data-prefix="lineNumber"
+      ><!--
+        --><code v-text="line" /><!--
+      --></pre>
+    </div>
   </div>
 </template>
 
@@ -23,6 +29,8 @@ export default {
 
   computed: {
     code: ({ entity }) => entity.attributes.field_code.split('\n'),
+    description: ({ entity }) =>
+      (entity.attributes.field_description || {}).processed,
     filename: ({ entity }) => entity.attributes.field_title,
   },
 }
