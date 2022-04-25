c38bde85bb8cda33c3d1d5455cf6671691aac243
Stuart Clark <stu@rtclark.net>
2022-04-25 10:39:27 +1000

    feat(#63): add a-frame component

---
+++ nuxt/components/druxt/entity/paragraph/aframe-scene/Default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="my-8">
+    <ClientOnly>
+      <!-- eslint-disable vue/no-v-html -->
+      <a-scene class="container h-96 mx-auto" embedded v-html="scene" />
+      <!-- eslint-enable vue/no-v-html -->
+    </ClientOnly>
+  </div>
+</template>
+
+<script>
+import { DruxtEntityMixin } from 'druxt-entity'
+
+// Import A-Frame library client-side only.
+if (process.client) {
+  require('aframe')
+}
+
+export default {
+  mixins: [DruxtEntityMixin],
+
+  computed: {
+    scene: ({ entity }) => entity.attributes.field_scene,
+  },
+}
+</script>
