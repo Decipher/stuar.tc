02cbf65326bdec4bab2c5e25d9dd40d0ba592518
Stuart Clark <stu@rtclark.net>
2022-06-17 02:08:02 +0000

    feat(#80): add druxt node preview

---
+++ nuxt/layouts/preview.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <Nuxt class="flex-1 overflow-y-auto" />
+  </div>
+</template>
+
+<script>
+import ConfigMixin from '~/mixins/config'
+
+export default {
+  mixins: [ConfigMixin],
+}
+</script>
