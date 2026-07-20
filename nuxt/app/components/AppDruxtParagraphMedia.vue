9f67c53a107c14f5b39a6e1e63963a784fdb4459
Stuart Clark <stu@rtclark.net>
2026-07-20 01:25:27 +0000

    fix(nuxt): stop cropping writing images, add click-to-zoom via SCImageLightbox

--- nuxt/app/components/AppDruxtParagraphMedia.vue
+++ nuxt/app/components/AppDruxtParagraphMedia.vue
@@ -5,16 +5,11 @@ defineProps<{ paragraph: Extract<Paragraph, { type: 'media' }> }>()
 </script>
 
 <template>
-  <figure>
-    <img
-      :src="paragraph.src"
-      :alt="paragraph.alt"
-      :width="paragraph.width"
-      :height="paragraph.height"
-      loading="lazy"
-      class="aspect-[4/3] w-full rounded-lg border border-default object-cover"
-    >
-    <!-- eslint-disable-next-line vue/no-v-html -->
-    <figcaption v-if="paragraph.caption" class="mt-2.5 text-center text-[13.5px] text-muted" v-html="paragraph.caption" />
-  </figure>
+  <SCImageLightbox
+    :src="paragraph.src"
+    :alt="paragraph.alt"
+    :width="paragraph.width"
+    :height="paragraph.height"
+    :caption="paragraph.caption"
+  />
 </template>
